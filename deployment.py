1059d92a6b20146aba9c65e56f4a69a66bc5abee
Foziljon <127921695+MisterFoziljon@users.noreply.github.com>
2023-08-31 00:58:47 +0500

    Add files via upload

---
+++ deployment.py
@@ -0,0 +1,109 @@
+from ultralytics import YOLO
+import streamlit as st
+import numpy as np
+import tempfile
+import torch
+import cv2
+import os
+
+class Deployment:
+    def __init__(self):
+        
+        self.helmet_model = "models/ppe.pt"
+        self.person_model = "models/person.pt"
+        
+        self.colors = [(255,0,0),(0,255,0),(0,0,255)]
+
+    def detect_helmet(self, image, model):
+        results = model.predict(image, conf=0.7, stream = True, classes=[0,2], device = "cuda:0")
+        
+        detection = None
+        for result in results:
+            boxes = result.boxes.cpu().numpy()
+            for box in boxes:
+                cls = box.cls.astype(int)
+                points = box.xyxy[0].astype(int)
+                color = self.colors[1]
+                selected_class = "Hardhat"
+
+                if cls[0]==2:
+                    color = self.colors[0]
+                    selected_class = "No Hardhat"
+                detection = [points,selected_class,color]
+            break
+        
+        return detection
+
+
+    def run(self, video_path):
+                
+        person_model = YOLO(self.person_model)
+        helmet_model = YOLO(self.helmet_model)
+        
+        FRAME_WINDOW = st.image([])
+        
+        video = cv2.VideoCapture(video_path)
+        
+        width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
+        height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        
+        class_ = {0: 'Hardhat', 2: 'No Hardhat'}
+        
+        while video.isOpened():
+            try:
+                ret, frame = video.read()
+                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                results = person_model.predict(frame, conf=0.7, stream = True, classes = [0], device = "cuda:0")
+                
+                for result in results:
+                    boxes = result.boxes.cpu().numpy()
+                    for box in boxes:
+                        points = box.xyxy[0].astype(int)
+                        xmin,ymin,xmax,ymax = points
+                        
+                        cv2.rectangle(frame, (xmin,ymin), (xmax,ymax), self.colors[-1], 2)
+                        cv2.putText(frame, "person", (xmin,ymin), cv2.FONT_HERSHEY_SIMPLEX, 1, self.colors[-1], 2, cv2.LINE_AA)
+                        
+                        person = frame[max(0,ymin-30):ymax, xmin:xmax]
+                            
+                        detection = self.detect_helmet(person, helmet_model)
+                                            
+                        if detection is not None:
+                            points, selected_class, color = detection
+                            
+                            xmin_ppe = xmin + points[0]
+                            ymin_ppe = ymin-30 + points[1]
+                            xmax_ppe = xmin + points[2]
+                            ymax_ppe = ymin-30 + points[3]
+
+                            cv2.rectangle(frame, (xmin_ppe,ymin_ppe), (xmax_ppe,ymax_ppe), color, 2)
+                            cv2.putText(frame, selected_class, (xmin_ppe,ymin_ppe), cv2.FONT_HERSHEY_SIMPLEX, 1, color, 2, cv2.LINE_AA)
+                        
+                if not ret or video.isOpened()==False:
+                    break
+
+                FRAME_WINDOW.image(frame)
+                
+            except:
+                break
+        video.release()
+
+
+if  __name__ == '__main__':
+    
+    st.title("Hardhat(Helmet) detection")
+    src = st.radio("Select video type: ", ["Sample video", "Upload video"])
+    deployment = Deployment()
+    
+    if src=="Sample video":
+        deployment.run("sample.mp4")
+
+    else:
+        file = st.file_uploader("Upload your file here...", type=["mp4", "avi", "mov"]) 
+        if file is not None:
+            video_path = None
+            with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+                video_path = temp_file.name
+                temp_file.write(file.read())
+                deployment.run(video_path)
+            os.remove(video_path)
